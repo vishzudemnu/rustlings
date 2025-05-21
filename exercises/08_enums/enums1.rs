0c34729ea5cfd0288c06efd3b5ecbbd473f23072
vishzudemnu <vishnuvardhan.viswanathan@gmail.com>
2025-05-21 16:54:25 +0200

    u

--- exercises/08_enums/enums1.rs
+++ exercises/08_enums/enums1.rs
@@ -1,7 +1,13 @@
 #[derive(Debug)]
 enum Message {
     // TODO: Define a few types of messages as used below.
+    Resize,
+    Move,
+    Echo,
+    ChangeColor,
+    Quit,
 }
+
 
 fn main() {
     println!("{:?}", Message::Resize);
